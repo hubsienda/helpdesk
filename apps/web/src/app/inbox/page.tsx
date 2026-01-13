8e37e5d3ad38d96baff01073b35eb8b4c3caeb2d
hubsienda <hub@siendaweblines.com>
2026-01-13 18:28:38 +0100

    Update page.tsx

--- apps/web/src/app/inbox/page.tsx
+++ apps/web/src/app/inbox/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { fetchTickets } from "@/lib/api";
+import { fetchTickets } from "../../lib/api";
 
 export default async function InboxPage() {
   let tickets = [];
